fb0f2169d2b3dc4a391838301885b2b0efb4e8d2
Daybson Paisante <daybson.paisante@outlook.com>
2019-05-06 12:47:20 -0300

    + Scene 001 - Hinge Joints.unity: robotic arm controlled by RigidBody forces

---
+++ UnityJoints3D/Assets/Scenes/Scene 001/Scene001.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene001 : MonoBehaviour
+{
+    public Rigidbody body;
+    public float Force;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    private void LateUpdate()
+    {
+        var z = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+        var input = new Vector3(0, y, z);
+
+        body.AddForce(input * Force * Time.smoothDeltaTime);
+    }
+}
