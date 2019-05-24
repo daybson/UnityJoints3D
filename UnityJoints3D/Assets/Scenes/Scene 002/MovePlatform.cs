983f65af25113d2c50feaefdbb97b06ae76c8b28
Daybson Paisante <daybson.paisante@outlook.com>
2019-05-24 12:50:20 -0300

    + tmp files, materiais,

---
+++ UnityJoints3D/Assets/Scenes/Scene 002/MovePlatform.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlatform : MonoBehaviour
+{
+    public float speed;
+    Rigidbody body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        body.MovePosition(transform.position + transform.right * speed * Time.fixedDeltaTime);
+    }
+}
